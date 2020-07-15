11f276db5d37f7c265dd043997acb68ba47a82e5
Anson Nickel <anson.nickel@gmail.com>
2020-07-15 18:21:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  hash = {name: "Anson"}
+  {name: "Anson"}
 end
 
 def pioneer
-  pioneer_hash = {name: "Grace Hopper"}
+  {name: "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  number_hash = {id: number}
+  {id: number}
 end
